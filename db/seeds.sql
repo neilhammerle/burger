795e81c13a585ed8a3a3d467faf8b2951020d0c0
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-01-23 03:09:52 -0800

    sql and server

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Club Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
