c0ebd4cb63e61a32173771d94c666630830b42f1
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-09-17 21:30:05 -0700

    adds cards

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,50 +1,49 @@
 import React from 'react'
 
 import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
-import ListGroupItem from 'react-bootstrap/ListGroupItem'
+import CardGroup from 'react-bootstrap/CardGroup'
+
+import Cheeseburger from '../../assets/images/cheeseburger.png'
+import Hamburger from '../../assets/images/hamburger.png'
+import ChickenSandwhich from '../../assets/images/chickensandwhich.png'
 
 const index = () => {
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src={Cheeseburger} alt="cheeseburger" />
+                    <Card.Body>
+                        <Card.Title>Amazing Burgers!</Card.Title>
+                        <Card.Text>
+                            Nelson's Gourmet Burgers had some of the best burgers I ever had.
+                            It was like unreal! - John Williams from San Francisco, CA
+      </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={Hamburger} alt="hamburger" />
+                    <Card.Body>
+                        <Card.Title>Best Burgers in the whole Bay Area!</Card.Title>
+                        <Card.Text>
+                            If you are visiting San Francisco Bay Area you must stop by Nelson's
+                            Gourmet Burgers. It was only a few blocks away from the BART station
+                            off Market St in San Francisco. A definitely a must stop! - Chris Smith from Los Angeles, CA
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={ChickenSandwhich} alt="chickensandwhich" />
+                    <Card.Body>
+                        <Card.Title>Incredible Food!</Card.Title>
+                        <Card.Text>
+                            Been going to Nelson's Groumet Burgers for years now. Whenever I'm
+                            close by it's a must stop always! Not only the burgers are delicious,
+                            but the chicken sandwhiches. - Michael Johnson from San Jose, CA
+      </Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
         </div>
     )
 }
